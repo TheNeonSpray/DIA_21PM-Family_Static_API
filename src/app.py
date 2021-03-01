44a69fe08b4e449c78e00dd4bd0dfbddb19da9cf
Alex Spinoza <alexandraeb20@gmail.com>
2021-03-01 21:23:06 +0000

    Entrega Family API

--- src/app.py
+++ src/app.py
@@ -7,8 +7,9 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
 #from models import Person
-
+#CORS me permite hacer un cruce de origen, sin importar donde este el fetch
 app = Flask(__name__)
+#Que no sea estricto con los slashes
 app.url_map.strict_slashes = False
 CORS(app)
 
@@ -22,21 +23,65 @@ def handle_invalid_usage(error):
 
 # generate sitemap with all your endpoints
 @app.route('/')
+#decoradores
 def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
-        "hello": "world",
         "family": members
     }
-
-
     return jsonify(response_body), 200
+
+
+@app.route('/members', methods=['POST'])
+def add_member():
+    new_member = request.get_json()
+    if isinstance(new_member, dict):
+        members = jackson_family.add_member(new_member)
+        response_body = {
+            "family": members
+        }
+        return jsonify(response_body), 200
+    else:
+        return jsonify({"msg":"400 Bad Request"}), 400
+
+
+@app.route('/members/<int:id>', methods=['DELETE'])
+def delete_member(id=None):
+    eliminado = jackson_family.delete_member(id)
+    if eliminado:
+        return jsonify({"msg": "El miembro fue removido"}), 200
+    else:
+        return jsonify({"msg":"Id no se reconoce"}), 404
+
+
+@app.route('/members/<int:id>', methods=['PUT'])
+def update_member(id=None):
+    updatemem = request.get_json()
+    modificado = jackson_family.update_member(id, updatemem)
+    if modificado:
+        return jsonify({"msg": "El miembro se ha actualizado"}), 200
+    else:
+        return jsonify({"msg":"400 Bad Request"}), 400
+
+
+@app.route('/members/<int:id>', methods=['GET'])
+def get_member(id):
+
+    member = jackson_family.get_member(id)
+    if member != None:
+        # response_body = {
+        #     "family": member
+        # }
+        return jsonify(member), 200
+    else: 
+        
+        return jsonify({"msg":"El id es inexistente"}), 404
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -13,23 +13,70 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+            {
+                "id": self._generateId(),
+                "first_name": "John",
+                "last_name": last_name,
+                "age": 33,
+                "lucky_numbers": [7,13,22]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Jane",
+                "last_name": last_name,
+                "age": 35,
+                "lucky_numbers": [10,14,3]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "lucky_numbers": [1]
+            }
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        member["id"] = self._generateId()
+        member["last_name"] = self.last_name
+        self._members.append(member)
+        return self._members
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        eliminado = False
+        for member in self._members:
+            if member["id"] == id: 
+                self._members.remove(member)
+                eliminado = True
+        return eliminado
+
+## loop the list and replace the member with the given id
+        #recorro la lista
+        # "x" se refiere al objeto al que quiero cambiar
+        # El "member" después del = se refiere al member que recibo como parametro
+
+        #En caso de la manera actual en funcionamiento, "x" es el objeto, ".update es un método/función" y "(member)" es el parámetro que recibo.
+    def update_member(self, id, member):
+        modificado = False
+        for x in self._members:
+            if x["id"] == id: 
+                x.update(member)
+                modificado = True
+            #    x["first_name"] = member["first_name"]
+            #    x["age"] = member["age"]
+            #    x["lucky_numbers"] = member["lucky_numbers"]
+            
+        return modificado
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"] == id: 
+                return member
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
